70fe60aecc23f32a53c64d06833a60ef0ffc4aa6
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-04-28 18:25:10 +0545

    update: used figlet and chalk

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,34 @@
 import express from 'express';
 import { connectDatabase } from '@database';
-import { PORT } from '@config';
+import { LOG_FORMAT, PORT } from '@config';
+import { IRoute } from '@interfaces/routes.interface';
+
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import { figletText } from '@utils/figlet.utils';
+import chalk from 'chalk';
+import { c_error, info, success, warning } from '@utils/chalk.utils';
 
 
 export class App {
     public app: express.Application;
     public port: number | string;
+    public developer: string = 'Kaushal Kishor Mishra';
 
-    constructor() {
+    constructor(routes: IRoute[]) {
         this.app = express();
         this.port = PORT ? parseInt(PORT) : 3000;
         this.initializeConnection();
+        this.initializeMiddlewares();
+        this.initializeRoutes(routes);
+    }
 
+    private initializeMiddlewares() {
+        this.app.use(morgan(LOG_FORMAT));
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(bodyParser.json());
+        this.app.use(cors());
     }
 
     private async initializeConnection() {
@@ -20,10 +37,17 @@ export class App {
 
     public listen() {
         this.app.get('/', (req, res) => {
-            res.send('Hello to my food delivery app backend!!!');
+            res.send(success('Hello to my food delivery app backend!!!'));
         })
-        this.app.listen(this.port, () => {
-            console.log(`App listening on the port ${this.port}!!!`);
+        this.app.listen(this.port, async () => {
+            console.log(success(`______________________ 🚀 App listening on the port ${this.port}!!! ✔️ ______________________\n`));
+            console.log(info(await figletText(`Food Delivery App`)))
+        });
+    }
+
+    private initializeRoutes(routes: IRoute[]) {
+        routes.forEach(route => {
+            this.app.use('/api', route.router);
         });
     }
 }
